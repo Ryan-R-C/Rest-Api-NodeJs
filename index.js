ec932f854e2f3df28056dfde2507580586cc40a4
ryang <ryan.r.c.339ac@gmail.com>
2021-10-02 14:17:49 -0300

    created a new route, services

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ const  express = require('express')
 const app = express()
 
 app.listen(3000, () => {
-    console.log("Working the server in the 3000 gate")
+    console.log("Working the server in the 3000 gate")})
+
+app.get('/', (req, res) => {
+    res.send('Runing the server, allright!  👍')
 })
+app.get('/services', (req, res) => {
+    res.send('You are on services route! 😊')
+})
+//It means that when there is no rout it is gonna return the function
+//req == request; res == response
