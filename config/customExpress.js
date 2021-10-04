2b2035f9ae3c243f3e25d627ce19d969895a00d4
ryang <ryan.r.c.339ac@gmail.com>
2021-10-04 06:25:39 -0300

    imported body-parser for data manipulation

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,13 @@
 const  express = require('express')
 const consign = require('consign')
+const bodyParser = require('body-parser')//treat the content that will be send for the api
 
 module.exports = () => {
 
     const app = express()
-    
+
+    app.use(bodyParser.urlencoded({extended: true}))//It could read many types of data as .json()
+    app.use(bodyParser.json())//
     consign()
         .include('controllers') //import all controllers which is in cotrollers folder
         .into(app) //imports to app
--- controllers/services.js
+++ controllers/services.js
@@ -3,7 +3,9 @@ module.exports = app => {
         res.send('You are on services route! 😊')
     //Now instead of send data this function will ask for data
     app.post('/services', (req, res) => {
+        /*If just console.log(req.body), it will be returned undefined. It must be treated, so body-parser is necessary*/
         res.send('You are on services and it doing a POST! 📬🏠')
+    //For for test this, visually it gonna be used POSTMAN
     })
     })    
 }
