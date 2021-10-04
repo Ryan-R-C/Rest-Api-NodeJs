1cf1bd67e773959c8f4c22df9062b02ee6e9d945
ryang <ryan.r.c.339ac@gmail.com>
2021-10-04 05:55:44 -0300

    created what the services will do when it necessary a POST

--- controllers/services.js
+++ controllers/services.js
@@ -1,5 +1,9 @@
 module.exports = app => {
     app.get('/services', (req, res) => {
         res.send('You are on services route! 😊')
+    //Now instead of send data this function will ask for data
+    app.post('/services', (req, res) => {
+        res.send('You are on services and it doing a POST! 📬🏠')
+    })
     })    
 }
