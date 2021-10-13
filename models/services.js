554c24bae65d804246c05dbbe4d7b6ae54e41d0e
ryang <ryan.r.c.339ac@gmail.com>
2021-10-12 22:19:30 -0300

    created how services will handle the data

--- models/services.js
+++ models/services.js
@@ -1,1 +1,19 @@
 //In this models will send to data to databse
+const connection = require('../infrastructure/connection')
+//Requires MySQL config
+
+class Services{
+    add(service){
+        const sql = `INSERT INTO Services SET ?`
+        //In query: sql config, the object that is beeing saved, error and results
+        connection.query(sql, service, (error, results) =>{
+            if(error){
+                console.log(error)
+            }
+            else{
+                console.log(results)
+            }
+        })
+
+    }
+}
