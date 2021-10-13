0b68d1c47c2f0fb2c5c1885dbe6c3fd112459185
ryang <ryan.r.c.339ac@gmail.com>
2021-10-12 22:27:59 -0300

    uses the models in services

--- controllers/services.js
+++ controllers/services.js
@@ -6,8 +6,8 @@ module.exports = app => {
     //Now instead of send data this function will ask for data
     app.post('/services', (req, res) => {
         //before send a res to client it is necessary to see what is happening with the req
-        console.log(req.body)//header -> htpp; body -> content type
-        
+        const service = req.body
+        Services.add(service)
         /*If just console.log(req.body), it will be returned undefined. It must be treated, so body-parser is necessary*/
         res.send('You are on services and it doing a POST! 📬🏠')
     //For for test this, visually it gonna be used POSTMAN
