c05008729674ecd145ff524130631e4c2763712f
ryang <ryan.r.c.339ac@gmail.com>
2021-10-16 13:33:06 -0300

    installed moment and handle dates

--- models/services.js
+++ models/services.js
@@ -4,9 +4,16 @@ const connection = require('../infrastructure/connection')
 
 class Services{
     add(service){
+        //All these dates are in American pattern
+        const date = moment().format('MMMM Do YYYY, h:mm:ss a'); //used to format the moment that the user create the data
+        dateCreation = moment(service.dateCreation).format('MMMM Do YYYY, h:mm:ss a');
+        //instead of change this object it will be created a new object:
+        const ServiceDated = {...service, date,dateCreation}
+        
+
         const sql = `INSERT INTO Services SET ?`
         //In query: sql config, the object that is beeing saved, error and results
-        connection.query(sql, service, (error, results) =>{
+        connection.query(sql, ServiceDated, (error, results) =>{
             if(error){
                 console.log(error)
             }
