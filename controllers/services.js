1f9e30563e8ced618c864e524a43aed62ca3f8dc
ryang <ryan.r.c.339ac@gmail.com>
2021-10-16 13:57:06 -0300

    handle more the dates and now the response is passed to services

--- controllers/services.js
+++ controllers/services.js
@@ -7,9 +7,9 @@ module.exports = app => {
     app.post('/services', (req, res) => {
         //before send a res to client it is necessary to see what is happening with the req
         const service = req.body
-        Services.add(service)
+        Services.add(service, res)
         /*If just console.log(req.body), it will be returned undefined. It must be treated, so body-parser is necessary*/
-        res.send('You are on services and it doing a POST! 📬🏠')
+
     //For for test this, visually it gonna be used POSTMAN
     })    
 }
--- models/services.js
+++ models/services.js
@@ -1,12 +1,14 @@
+
+
 //In this models will send to data to databse
 const connection = require('../infrastructure/connection')
 //Requires MySQL config
 
 class Services{
-    add(service){
+    add(service, res){
         //All these dates are in American pattern
-        const date = moment().format('MMMM Do YYYY, h:mm:ss a'); //used to format the moment that the user create the data
-        dateCreation = moment(service.dateCreation).format('MMMM Do YYYY, h:mm:ss a');
+        const date = moment().format('YYYY-MM-DD hh:mm:ss a'); //used to format the moment that the user create the data | On MYSQL date format
+        dateCreation = moment(service.dateCreation, 'DD/MM/YYYY').format('MMMM Do YYYY, h:mm:ss a')//THE second param in moment is just for dev porpouses.
         //instead of change this object it will be created a new object:
         const ServiceDated = {...service, date,dateCreation}
         
@@ -15,10 +17,10 @@ class Services{
         //In query: sql config, the object that is beeing saved, error and results
         connection.query(sql, ServiceDated, (error, results) =>{
             if(error){
-                console.log(error)
+                res.status(400).json(error)
             }
             else{ 
-                console.log(results)
+                res.status(201).json(results)
             }
         })
 
