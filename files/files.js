937ff341a77e40f2bf2c46b35240582c31137f17
Ryan Rodrigo Costa <ryan.r.c.339ac@gmail.com>
2021-12-03 22:57:59 -0300

    commented files.js

--- files/files.js
+++ files/files.js
@@ -1,5 +1,16 @@
 const fs = require('fs')
+/*
+Now it is necessary to handle images
 
+make a upload of image through a buffer
+*/
 fs.readFile('../assets/', (error, buffer) => {
-
+    
 } )
+/*
+fs.readFile ->
+ first argument is the path to file
+ the second some more config like type of file
+ and the third is a callBack function
+    and with arguments -> (error, buffer)
+*/
